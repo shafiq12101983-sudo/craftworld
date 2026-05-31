5b35fe9158b8d7d380b1c608563cd945b321726d
Shafiq Ahmad <shafiq12101983@gmail.com>
2026-05-31 19:26:17 +0500

    Database Migration

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ db.init_app(app)
 # CONFIG
 # ═══════════════════════════════════════════
 UPLOAD_FOLDER      = "uploads"
-ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "webp"}
+ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "webp"}git
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
